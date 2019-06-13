e7588634552b00d29f4323cdcff8837974f12d14
ncq <54173388@qq.com>
2019-06-13 17:27:45 +0800

    展示权限管理--权限列表

--- src/components/quanxian/quanxianlist.vue
+++ src/components/quanxian/quanxianlist.vue
@@ -9,11 +9,19 @@
     <!-- 使用封装的自定义组件 -->
     <my-bread lev1="权限管理" lev2="权限列表"></my-bread>
     <!-- 表格 -->
-    <el-table :data="tableData" stripe style="width: 100%" height=430>
+    <el-table :data="rights_list" stripe style="width: 100%" height="430" border>
       <el-table-column type="index" width="50"></el-table-column>
       <el-table-column prop="authName" label="权限名称" width="180"></el-table-column>
       <el-table-column prop="path" label="路径" width="180"></el-table-column>
-      <el-table-column prop="level" label="层级"></el-table-column>
+      <el-table-column label="层级">
+        <template slot-scope="scope">{{level_list[scope.row.level]}}</template>
+        // 或者用v-if 实现
+        <!-- <template slot-scope='scope'>
+          <span v-if="+scope.row.level === 0">一级</span>
+          <span v-if="scope.row.level === '1'">二级</span>
+          <span v-if="+scope.row.level === 2">三级</span>
+        </template> -->
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -22,7 +30,8 @@
 export default {
   data() {
     return {
-      tableData: []
+      rights_list: [],
+      level_list: ['一级', '二级', '三级']
     }
   },
   created() {
@@ -32,9 +41,9 @@ export default {
     async get_rights_list() {
       const res = await this.$http.get('rights/list')
       // console.log(res)
-      const {msg, status} = res.data.meta
+      const { msg, status } = res.data.meta
       if (status === 200) {
-        this.tableData = res.data.data
+        this.rights_list = res.data.data
       }
     }
   }
