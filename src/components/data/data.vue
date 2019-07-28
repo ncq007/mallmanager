4da5d2ee1189642c558d3d58f20257b291fd69b1
ncq <ncq007@qq.com>
2019-07-29 07:15:44 +0800

    add echarts

--- src/components/data/data.vue
+++ src/components/data/data.vue
@@ -1,13 +1,63 @@
 <template>
-  <el-card class="box-card">数据管理</el-card>
+  <el-card class="box-card">
+    <my-bread
+      lev1="数据统计"
+      lev2="数据报表"
+    ></my-bread>
+    <div
+      id="main"
+      style="width: 600px;height: 400px;"
+    ></div>
+  </el-card>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
+  data () {
+    return {
 
+    }
+  },
+  mounted () {
+    this.useEchart()
+  },
+  methods: {
+    async useEchart () {
+      let myChart = echarts.init(document.getElementById('main'))
+      const res = await this.$http.get('reports/type/1')
+      let option1 = {
+        title: {
+          text: '堆叠区域图'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#6a7985'
+            }
+          }
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        }
+      }
+      let option2 = res.data.data
+      let option = { ...option1, ...option2 }
+      myChart.setOption(option)
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
